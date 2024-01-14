934297cc891d2d5be2e5d47237ef2cecdaab0e9c
vinz321 <vincenzo.micciche.27@gmail.com>
2024-01-14 16:09:29 +0100

    Structs fix

--- BlackHoleSim/sphere.cpp
+++ BlackHoleSim/sphere.cpp
@@ -1,7 +1,7 @@
 #include "sphere.h"
 
 __host__ __device__ bool sphere::is_inside(vec3 point) {
-	int a, b, c;
+	float a, b, c;
 	a = origin.x - point.x;
 	b = origin.y - point.y;
 	c = origin.z - point.z;
---
+++ BlackHoleSim/structs.cpp
@@ -0,0 +1,28 @@
+#include "structs.h"
+
+__host__ __device__ vec3 operator+ (const vec3& x, const vec3& y) {
+	return vec3{ x.x + y.x,
+		x.y + y.y,
+		x.z + y.z };
+}
+
+__host__ __device__ float operator* (const vec3& x, const vec3& y) {
+	return x.x * y.x +
+		x.y * y.y +
+		x.z * y.z;
+}
+
+__host__ __device__ vec3 operator- (const vec3& x, const vec3& y) {
+	return vec3{ x.x - y.x,
+		x.y - y.y,
+		x.z - y.z };
+}
+
+__host__ __device__ vec3 operator/ (const vec3& x, const float& y) {
+	return vec3{ x.x / y,
+		x.y / y,
+		x.z / y };
+	}
+__host__ __device__ vec3 norm(const vec3 & v) {
+	return v / sqrtf(v * v);
+}
