23c44c79654f56ea141b78707c678c6a39f4df19
Koidrab <gio.violo1@gmail.com>
2024-01-26 17:15:17 +0100

    semi-funzionante palle rotazione hdri

--- BlackHoleSim/camera.h
+++ BlackHoleSim/camera.h
@@ -3,4 +3,4 @@
 #include "structs.h"
 #include "ray.h"
 
-__host__ __device__ camera_t make_cam(vec3_t lookfrom, vec3_t lookat, vec3_t vup, float vfov, float aspect);
+__host__ __device__ camera_t make_cam(vec3_t lookfrom, vec3_t lookat, vec3_t vup, float vfov, float aspect);    vec3_t vertical;}
